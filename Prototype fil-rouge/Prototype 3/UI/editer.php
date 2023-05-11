7b8d70d9d796e1365069436debf6be8549747c96
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-05-11 13:27:00 +0100

    working on prototype level 3

--- Prototype fil-rouge/Prototype 3/UI/editer.php
+++ Prototype fil-rouge/Prototype 3/UI/editer.php
@@ -1,19 +1,20 @@
 <?php
 
 // include "GestionProject.php";
-include "../Managers/GestionTask.php";
-$GestionTasks = new GestionTasks();
+include "../Managers/GestionProject.php";
+
+$GestionProjects = new GestionProjects();
 
 if (isset($_GET['id'])) {
-    $project = $GestionTasks->RechercherParId($_GET['id']);
+    $project = $GestionProjects->RechercherParId($_GET['id']);
 }
-$id_projet = $_GET['id_project'];
+
 if (isset($_POST['modifier'])) {
     $id = $_POST['Id'];
     $Name = $_POST['Name'];
     $Description = $_POST['Description'];
-    $GestionTasks->Modifier($id, $Name, $Description);
-    header("Location: Tasks.php?id=".$id_projet);
+    $GestionProjects->Modifier($id, $Name, $Description);
+    header('Location: index.php');
 }
 ?>
 <!DOCTYPE html>
@@ -23,15 +24,13 @@ if (isset($_POST['modifier'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Style/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Modifier : </title>
 </head>
 
 <body>
 
-    <h1>Modification de project :
-        <?= $project->getName() ?>
-    </h1>
+    <h1>Modification de project : <?= $project->getName() ?></h1>
     <form method="post" action="">
         <input type="text" required="required" id="Id" name="Id" value=<?php echo $project->getId() ?>>
 
@@ -41,8 +40,7 @@ if (isset($_POST['modifier'])) {
         </div>
         <div>
             <label for="Prenom">Description</label>
-            <input type="text" required="required" id="Description" name="Description" placeholder="Description"
-                value=<?php echo $project->getDescription() ?>>
+            <input type="text" required="required" id="Description" name="Description" placeholder="Description" value=<?php echo $project->getDescription()?>>
         </div>
         <div>
             <input name="modifier" type="submit" value="Modifier">
