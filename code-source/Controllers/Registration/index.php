072be213f792dacfda9512f64afdaf43049ef866
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-05-29 14:52:52 +0100

    Update index.php

--- code-source/Controllers/Registration/index.php
+++ code-source/Controllers/Registration/index.php
@@ -3,14 +3,14 @@ include "../../Views/Layout/root.php";
 if (isset($_POST['Login'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $user = new User();
-    $user->login($email, $password);
+    // $user = new User();
+    // $user->login($email, $password);
 }else if(isset($_POST['Register'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $user = new User();
-    $user->register($name, $email, $password);
+    // $user = new User();
+    // $user->register($name, $email, $password);
 }
 include_once(__ROOT__ . "/Views/Registration/index.html")
     ?>
